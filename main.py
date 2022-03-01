9c09a7a7e104d64ab632bccd0f06cded3b61a6da
Julian Marin <julian.marinc2@gmail.com>
2022-03-01 15:08:34 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 
+#python
+
+from typing import Optional
+#pydantic
+from pydantic import BaseModel
+
+#fastapi
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#models
+
+class Person (BaseModel):
+    firt_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
 def home():
     return{"Hello":"word"}
+
+#request and response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
